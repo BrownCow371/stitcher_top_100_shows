b747aef91e968c1c5a968f66d51cd4ece7f2010a
Jamie Brown <jmbrowncow@gmail.com>
2018-07-20 13:26:40 +0000

    adding hard coded functionality to cli

--- lib/stitcher_top_100_shows/cli.rb
+++ lib/stitcher_top_100_shows/cli.rb
@@ -2,18 +2,28 @@ class StitcherTopShows::CLI
 
   def call
     puts "Welcome to the weekly ranking of the top 100 most popular podcast shows on Stitcher!"
-    #start? or pick category?
+    self.pick_category
   end
 
   def pick_category
-    puts "Which category of shows would you like to see the top 100 for?"
+    puts "Which category of shows would you like to see the top 100 for? Enter the category list number."
+    self.print_categories
+    input = gets.strip.to_i
 
+    
   end
 
   def pick_show(url)#might need url argument?
   end
 
-  def print_categories(category)
+  def print_categories
+    #hard code some categories here. 
+    puts "----------------------------------"
+    puts "1. All" 
+    puts "2. Comedy"
+    puts "3. Business and Industry"
+    puts "4. News and Politics"
+    puts "5. Society and Culture"
   end
 
   def print_shows(range)
@@ -22,4 +32,12 @@ class StitcherTopShows::CLI
   def print_show_details(show)
   end
 
+  def back_to_show_list
+    
+  end
+
+  def back_to_category_list
+    StitcherTopShows::Show.reset
+    self.pick_category
+  end
 end
