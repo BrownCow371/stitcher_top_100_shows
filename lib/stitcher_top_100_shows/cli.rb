3c3727f06b0ae47a43b7878e0ac2aa433c6fab39
Jamie Brown <jmbrowncow@gmail.com>
2018-07-20 14:51:55 +0000

    Adding back logic to pick_show method

--- lib/stitcher_top_100_shows/cli.rb
+++ lib/stitcher_top_100_shows/cli.rb
@@ -24,10 +24,36 @@ class StitcherTopShows::CLI
 
   #def pick_show(url)#might need url argument?
   def pick_show
-    puts "Which show would you like more details on? Please Enter the Show List number."
-    input = gets.strip.to_i
-    show = StitcherTopShows::Show.find_by_index(input)
-    print_show_details(show)
+    puts "Which show would you like more details on?"
+    puts "Please Enter the Show List number or 'Back' to return to the Category list."
+    input = gets.strip
+    if input.downcase == "back"
+      self.back_to_category_list
+    else
+      show = StitcherTopShows::Show.find_by_index(input.to_i)
+      print_show_details(show)
+
+      puts ""
+      puts "Would you like to see another show?"
+      puts "Enter Y if you do, 'Exit' if you are done of 'Back' if you would like to got back to the Category List."
+
+      input = gets.strip.downcase
+
+        if input == "y" 
+          # self.print_show - maybe want this?
+          self.pick_show
+        elsif input == "back"
+          self.back_to_category_list
+        elsif input == "exit"
+          puts "Thank you for using Stitcher Top 100 Shows!"
+          puts "Have a great day!"
+          exit
+        else 
+          puts ""
+          puts "I don't understand that answer."
+          self.pick_show
+        end
+    end
   end
 
   def print_categories
@@ -53,11 +79,20 @@ class StitcherTopShows::CLI
   end
 
   def print_show_details(show)
-    
+    puts ""
+    puts "---------------------Show Details---------------------"
+    puts "Title:                     The Show"
+    puts "About:                     Stuff about the show"
+    puts "Category:                  Comedy"
+    puts "Stars:                     4.0"
+    puts "Most Recent Episode:       The funny thing about Ruby"
+    puts "Most Recent Episode Desc:  A podcast about Ruby"
+    puts "Most Recent Episode URL:   somewhere on Stitcher"
+    puts "-----------------------------------------------------"
+
   end
 
   def back_to_show_list
-    
   end
 
   def back_to_category_list
